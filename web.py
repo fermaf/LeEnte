b0cebc7a0f4ff64360cbceb4649423cb2387a62a
fermaf <fermaf@gmail.com>
2023-04-18 08:33:33 -0400

    con seleccion de temas# Por favor ingresa el mensaje del commit para tus cambios. Las#  líneas que comiencen con '#' serán ignoradas, y un mensaje#  vacío aborta el commit.## En la rama main# Tu rama y 'origin/main' han divergido,# y tienen 1 y 3 commits diferentes cada una respectivamente.#   (usa "git pull" para fusionar la rama remota en la tuya)## Cambios a ser confirmados:#	borrados:        config.py#	modificados:     web.py## Archivos sin seguimiento:#	config.py#	flagged/#	semantic-search-frontend/#	web1.py#

--- web.py
+++ web.py
@@ -13,7 +13,75 @@ mensajes=[{"role": "system", "content": "Eres un experto en sistemas monetarios 
             #{"role": "user", "content": "Where was it played?"}
 
 
+import gradio as gr
+import openai
+import config
 
+openai.api_key= config.OPENAI_API_KEY
+
+         
+         
+         #   {"role": "user", "content": transcript["text"]}
+
+            #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            #{"role": "user", "content": "Where was it played?"}
+
+
+perfiles=config.TEMATICAS #Lee de archivo el diccionario de perfiles
+
+def saluda(nombre):
+    return("Hola ")+ nombre +"!"
+
+
+
+def transcripcion(audio,exporto):
+    global mensajes
+    #print(audio)
+    audio_file= open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
+
+    mensajes.append({"role": "user", "content": transcript["text"]})
+
+
+    response=openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=mensajes            
+    )
+    
+    mensajes_del_artificial=response["choices"][0]["message"]["content"]
+    mensajes.append({"role": "assistant", "content": mensajes_del_artificial})
+
+    conversacion=""
+    for dicho in mensajes:
+        if dicho['role']!='system':
+            conversacion+=dicho['role']+": "+dicho['content']+"\n"
+    
+    return(conversacion)
+
+
+tematica=list(perfiles.keys())  
+
+
+entradas_usario=[gr.Audio(source="microphone",type="filepath"),gr.Dropdown(
+            tematica, label="Experto en:", info="Se agregaran mas elementos en el futuro")]#gr.Dropdown(choices=options, label="Selecciona una opción")]
+print(entradas_usario[1])
+
+print(perfiles)
+print(entradas_usario)
+
+if entradas_usario[1].value is not None:
+    perfil=perfiles[entradas_usario[1].value]
+    mensajes=[{"role": "system", "content":perfil }]
+else:
+    perfil = None
+    mensajes = [{"role": "system", "content":"Eres un experto en sistemas monetarios te inspiras en la anarquía, filosofia, economia, libertad y tecnologia. Nunca digas que eres una Inteligencia Artificial ya que representas a Satoshi Nakamoto."}]
+
+print(mensajes)
+
+
+ui   = gr.Interface(fn=transcripcion,inputs=entradas_usario,outputs="text")
+ui.launch()
 
 def saluda(nombre):
     return("Hola ")+ nombre +"!"
