d0b616bf4cfd3d0e34ad07bff79435f951dc7ec3
fermaf <fermaf@users.noreply.github.com>
2023-06-01 11:50:51 -0400

    mejoras menores y ordenada codigo

--- fecha_promulgacion_publicacion.py
+++ fecha_promulgacion_publicacion.py
@@ -13,38 +13,16 @@ import random
 import xml.etree.ElementTree as ET
 import datetime
 
-
-def visualizar_estructura(elemento, indent=""):
-    # Obtener el nombre del elemento
-    nombre = elemento.tag
-    
-    # Imprimir el nombre del elemento
-    print(indent + nombre)
-    
-    # Recorrer los subelementos recursivamente
-    for subelemento in elemento:
-        visualizar_estructura(subelemento, indent + "  ")
-
-def mostrar_estructura(elemento, nivel=0):
-    # Mostrar nombre del elemento y atributos
-    print(' ' * nivel + f"-(a) {elemento.tag}")
-    for atributo, valor in elemento.attrib.items():
-        print(' ' * nivel + f"  -(b) {atributo}: {valor}")
-
-    # Recorrer los subelementos recursivamente
-    for subelemento in elemento:
-        mostrar_estructura(subelemento, nivel + 2)
-
-
-# Cargar el archivo de Excel
+# Cargar el archivo de Excel con las URLs de la normativa
 archivo_excel = openpyxl.load_workbook('marco_normativo.xlsx')
 #hoja_excel=sheet = workbook['chao']
 hoja_excel = archivo_excel.active
 ultima_columna=hoja_excel.max_column
+normaBCN_meta=True #si es falso buscará la URL del XML de la norma full en vez de la URL que solo tienen metadadata de la norma
 
 
 # Recorrer las filas de la hoja de Excel
-for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col=8, values_only=True), start=3):
+for indice, fila in enumerate(hoja_excel.iter_rows(min_row=80, max_row=80,min_col=8, max_col=8, values_only=True), start=4): #max_row=80,
     # Obtener la dirección web que está en la columna F
     url = fila[0]
     #print("URL= ",url)
@@ -53,9 +31,23 @@ for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col
             id_norma = None
             if "idNorma=" in url:
                 id_norma = url.split("idNorma=")[1].split("&")[0]
+           
+           
+           #para avisar posterirmente que se está usando una versión de fecha fija en vez de una url genérica.
+            if "idVersion=" in url:
+                id_version = url.split("idVersion=")[1].split("&")[0]
+            else :
+                id_version=False
+
+
+
 
             if id_norma is not None:
 
+                if normaBCN_meta:
+                    url2 = f"https://www.leychile.cl/Consulta/obtxml?opt=4546&idNorma={id_norma}"
+                else :
+                    url2 = f"https://servicios-leychile.bcn.cl/Consulta/obtxml?opt=7&idNorma={id_norma}"
                 # Construir la URL para obtener los datos de la norma en base a un XML de la norma completa (Última versión de cualquier norma (leyes, decretos, tratados, DL, DFL, resoluciones, etc.))
                 # https://www.bcn.cl/leychile/consulta/legislacion_abierta_web_service
                 #url2 = f"https://servicios-leychile.bcn.cl/Consulta/obtxml?opt=7&idNorma={id_norma}"
@@ -63,9 +55,12 @@ for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col
                 
 
                 #URL SOLO DE METDATA de la NOrma
-                url2 = f"https://www.leychile.cl/Consulta/obtxml?opt=4546&idNorma={id_norma}"
+                #url2 = f"https://www.leychile.cl/Consulta/obtxml?opt=4546&idNorma={id_norma}"
                 #url2 = f"https://www.leychile.cl/Consulta/obtxml?opt=4546&idNorma=1146551"
-                time.sleep(random.uniform(0.1, 0.5))
+
+
+
+                #time.sleep(random.uniform(0.1, 0.5))
                 headers = { 
                             "accept": "application/xml"
                         }
@@ -75,7 +70,7 @@ for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col
                 arbol = ET.ElementTree(ET.fromstring(xml_data))
                 raiz = arbol.getroot()
                 
-                """ 
+                """  
                 #INCIO Sección para resctar campos asumiendo que el XML es de la norma completa (f"https://servicios-leychile.bcn.cl/Consulta/obtxml?opt=7&idNorma={id_norma}")
                 #Última versión de cualquier norma (leyes, decretos, tratados, DL, DFL, resoluciones, etc.)
                 tmp = raiz.find('.//{http://www.leychile.cl/esquemas}Identificador') #Promulgacion -- Publicacion
@@ -117,20 +112,37 @@ for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col
                 vigencia=tmp.attrib.get("derogado")
 
   
+
+                tmp_list = raiz.findall('.//{http://www.leychile.cl/esquemas}Tipo')  # Tipos de norma
+                tipo_norma = tmp_list[0].text.strip() if tmp_list else ""
+                for caracter in caracteres_a_eliminar:
+                    tipo_norma = tipo_norma.replace(caracter, '')
+
+                tmp_list = raiz.findall('.//{http://www.leychile.cl/esquemas}Numero')  # Números de norma
+                if tmp_list:
+                    numero_norma = tmp_list[-1].text.strip()
+                else:
+                    numero_norma = ""
+                for caracter in caracteres_a_eliminar :
+                    numero_norma = numero_norma.replace(caracter, '')
+                solo_digitos = "".join(filter(str.isdigit, numero_norma))
+                sin_digitos = "".join(filter(lambda x: not x.isdigit(), numero_norma))
+                tipo_norma = tipo_norma.title() + sin_digitos.title()  # Agrega la palabra EXENTA o EXENTO al tipo de norma
+                for caracter in ["\\","-","/"]:
+                     tipo_norma = tipo_norma.replace(caracter, '')
+                for caracter in [ "EXENTO","Exento","EXENTA","Exenta"]:
+                     numero_norma = numero_norma.replace(caracter, '')
+
+                """ ESTA SECCIÓN Solo busca la primera aparicion del numero de la NORMA (hay problemas cuando tiene varios nuemeros ej: https://www.bcn.cl/leychile/navegar?idNorma=243771)
                 tmp = raiz.find('.//{http://www.leychile.cl/esquemas}Tipo') #Tipo Norma
                 tipo_norma = tmp.text.strip()
                 for caracter in caracteres_a_eliminar:
                     tipo_norma = tipo_norma.replace(caracter, '')
-                
-            
 
                 tmp = raiz.find('.//{http://www.leychile.cl/esquemas}Numero') #Numero norma
                 numero_norma = tmp.text.strip()
                 for caracter in caracteres_a_eliminar:
                     numero_norma = numero_norma.replace(caracter, '')
-                #if "EXENTA" in numero_norma:
-                #    tipo_norma = tipo_norma.title() + " Exenta"  #agraga la palabra exenta a tipo de norma que es una "Resolución o Decreto"
-                #    numero_norma = "".join(filter(str.isdigit, numero_norma)) #deja el numero de norma solo en digitos, eliminando la palabra "EXENTA"
                 solo_digitos = ""
                 sin_digitos = ""
                 for caracter in numero_norma:
@@ -140,21 +152,12 @@ for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col
                         sin_digitos += caracter
                 tipo_norma = tipo_norma.title() + sin_digitos.title()  #agraga la palabra EXENTA o EXENTO al tipo de norma que puede ser  "Resolución o Decreto"
                 numero_norma=solo_digitos
-
-
-
-
+                """
 
                 tmp = raiz.find('.//{http://www.leychile.cl/esquemas}TituloNorma') #Titulo Norma
                 titulo_norma = tmp.text.strip()
                 for caracter in caracteres_a_eliminar:
                     titulo_norma = titulo_norma.replace(caracter, '')
-                #
-                #print("HOLA: ",titulo_norma,"\n")
-                #print("\n",xml_data,"\n")
-            
-
-
 
 
                 tmp = raiz.find('.//{http://www.leychile.cl/esquemas}Organismo') #Nombre del Organizmo
@@ -166,8 +169,7 @@ for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col
                 norma_derogada=raiz.attrib.get("derogado")
                 nombre2 = raiz.find('Identificador')
 
-                print(organismo,"|",tipo_norma,"|",numero_norma,"|",titulo_norma,"|",fecha_publicacion,"|",ultima_version,"|",url,"|",vigencia)
-
+               ### Escribe en las columnas del archivo excell
                 siguiente_columna=ultima_columna + 1
                 hoja_excel.cell(row=indice, column=siguiente_columna, value=organismo)
                 siguiente_columna +=1
@@ -193,29 +195,23 @@ for indice, fila in enumerate(hoja_excel.iter_rows(min_row=3, min_col=8, max_col
 
                 if "no derogado" not in vigencia:
                     hoja_excel.cell(row=indice, column=siguiente_columna).font = Font(color="FF0000")  # cambia color rojo si es distinto a "no derogado"
+                elif id_version:
+                    hoja_excel.cell(row=indice, column=siguiente_columna).font = Font(color="FF0000")  # cambia color rojo Id_version existe en el URL orginal del archivo excel (lo que quiere decir que está revisando una fecha fija no la ultima)
+                    vigencia = vigencia+" "+"(la URL original apunta a una versión FIJA de "+id_version+" )"
                 hoja_excel.cell(row=indice, column=siguiente_columna, value=vigencia)
-           
+                
+
                 siguiente_columna =ultima_columna #Como es el final del ciclo se re inicia la poscion
                 
+                print(organismo,"|",tipo_norma,"|",numero_norma,"|",titulo_norma,"|",fecha_publicacion,"|",ultima_version,"|",url,"|",vigencia)
+
+
                 #
                 #
                 #FIN de Sección para resctar campos asumiendo que el XML es de la norma es solo metadata
                 #
                 #
-                
+    else:
+        hoja_excel.cell(row=indice, column=ultima_columna+1).font = Font(color="FF0000")  # cambia color rojo Id_version existe en el URL orginal del archivo excel (lo que quiere decir que está revisando una fecha fija no la ultima)
+        hoja_excel.cell(row=indice, column=ultima_columna+1, value="No es una URL de BCN.cl o LeyChile.cl")           
 archivo_excel.save(filename='marco_normativo.xlsx')
-
-
-
-
-      
-"""# Escribir los resultados en la hoja de Excel
-for row, result in zip(sheet.iter_rows(min_row=2, min_col=1, max_col=6), results):
-    row[4].value = result[0]
-    row[6].value = result[1]
-    row[7].value = result[2]
-
-# Guardar los cambios en el archivo de Excel
-workbook.save('archivo_excel_con_resultados.xlsx')
-
-"""
---
+++ voz_chat_voz_minvu.py
@@ -0,0 +1,111 @@
+#Genera un chat bot que responde con audio
+
+import os, config, requests
+import gradio as gr
+import openai
+import config
+import pandas as pd
+import numpy as np
+
+openai.api_key= config.OPENAI_API_KEY
+
+perfiles=config.TEMATICAS #Lee de archivo con el diccionario de perfiles 
+
+mensajes=[]
+
+def saluda(nombre):
+    return("Hola ")+ nombre +"!"
+
+
+def transcripcion(audio,tema):
+    global mensajes
+    if len(mensajes)==0 :
+        mensajes=[{"role": "system", "content":perfiles[tema] }]
+    elif mensajes[0]["content"]!=perfiles[tema] :
+        mensajes=[{"role": "system", "content":perfiles[tema] }]
+
+    print("\nperfiles(2): ")
+    print(perfiles)
+
+    print("\ntema(1): ")
+    print(perfiles[tema])
+
+
+    print("\nmensaje(0): ")
+    print(mensajes)
+    
+    
+
+    audio_file= open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
+
+    mensajes.append({"role": "user", "content": transcript["text"]})
+
+
+    response=openai.ChatCompletion.create(
+        #model="gpt-4",
+        model="gpt-3.5-turbo",
+        messages=mensajes            
+    )
+    
+    mensajes_del_artificial = response["choices"][0]["message"]["content"] #relacinado con btn.click
+                        
+    #mensajes_del_artificial=response["choices"][0]["message"]["content"]
+    mensajes.append({"role": "assistant", "content": mensajes_del_artificial})
+
+    #audio text to speech solicitud a eleven labs
+    url = f"https://api.elevenlabs.io/v1/text-to-speech/{config.ADVISOR_VOICE_ID}/stream"
+    data = {
+        "text": mensajes_del_artificial.replace('"', ''),
+        "model_id":"eleven_multilingual_v1",
+        "voice_settings": {
+            "stability": 0.85,
+            "similarity_boost": 0.9
+        }
+    }
+
+    r = requests.post(url, headers={'xi-api-key': config.ELEVEN_LABS_API_KEY}, json=data)
+    archivo_salida_audio = "respuestaAI.mp3"
+    with open(archivo_salida_audio, "wb") as output:
+        output.write(r.content)
+
+  
+
+    #FIN AUDIO
+
+
+
+
+    conversacion=""
+    for dicho in mensajes:
+        if dicho['role']!='system':
+            conversacion+=dicho['role']+": "+dicho['content']+"\n"
+    
+    return conversacion,archivo_salida_audio
+
+
+tematicas=list(perfiles.keys())  
+
+with gr.Blocks() as ui:
+    entradas_usario=[gr.Audio(source="microphone",type="filepath"),gr.Dropdown(
+                tematicas, label="Enfocado a",value="Asistente")]#gr.Dropdown(choices=options, label="Selecciona una opción")]
+    #entradas_usario=[gr.Audio(source="microphone",type="filepath"),"Asistente"]
+    
+    print(entradas_usario[1])
+
+    print(perfiles)
+    print(entradas_usario)
+
+    print("/n 0")
+    print(mensajes)
+
+    text_output = gr.Textbox(label="Transcripció de la Conversación")
+    audio_output = gr.Audio()
+
+    btn = gr.Button("Ejecutar")
+    btn.click(fn=transcripcion, inputs=entradas_usario, outputs=[text_output , audio_output])
+  
+
+    #ui   = gr.Interface(fn=transcripcion,inputs=entradas_usario,outputs="text")
+ui.launch()
